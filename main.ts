bd0002cfd358cd7263eaf959f688eef84fe1b202
2008-310272 <91247087+2008-310272@users.noreply.github.com>
2021-10-07 16:47:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,24 @@
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    led.setBrightness(0)
+})
+input.onButtonPressed(Button.AB, function () {
     for (let Y = 0; Y <= 4; Y++) {
         for (let X = 0; X <= 4; X++) {
             led.plot(X, Y)
             basic.pause(100)
         }
     }
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    led.setBrightness(0)
-})
-input.onButtonPressed(Button.B, function () {
     for (let Y = 0; Y <= 4; Y++) {
         for (let X = 0; X <= 4; X++) {
             led.plotBrightness(Y, 4 - X, 255)
             basic.pause(100)
         }
     }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    led.setBrightness(255)
 })
 basic.forever(function () {
     for (let Y = 0; Y <= 4; Y++) {
