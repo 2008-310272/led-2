6f1e77886c61ed4eb489b74739a43e7f8b01cffc
2008-310272 <91247087+2008-310272@users.noreply.github.com>
2021-10-07 15:58:37 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,41 @@
 basic.forever(function () {
     for (let Y = 0; Y <= 4; Y++) {
         for (let X = 0; X <= 4; X++) {
-            led.plot(4 - X, Y)
+            led.plotBrightness(4 - X, Y, 21)
             basic.pause(100)
         }
         for (let X = 0; X <= 4; X++) {
             led.plot(Y, 4 - X)
+            basic.pause(100)
+        }
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plotBrightness(4 - Y, X, 21)
+            basic.pause(100)
+        }
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, 4 - Y)
+            basic.pause(100)
+        }
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plotBrightness(X, 4 - Y, 21)
+            basic.pause(100)
+        }
+        for (let X = 0; X <= 4; X++) {
+            led.plot(4 - Y, X)
+            basic.pause(100)
+        }
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plotBrightness(Y, 4 - X, 21)
+            basic.pause(100)
+        }
+        for (let X = 0; X <= 4; X++) {
+            led.plot(4 - X, Y)
             basic.pause(100)
         }
     }
